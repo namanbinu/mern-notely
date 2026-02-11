f04e17ed9294165d80518faadfba94930a49ac74
Naman1369 <naman.binu.s73@kalvium.community>
2026-02-11 22:17:05 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import path from "path";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
 
@@ -9,13 +10,17 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve();
 
 
 //Middleware
-app.use(
+if(process.env.NODE_ENV !== "production") {
+    app.use(
     cors({
         origin: "http://localhost:5173",
     }))
+}
+
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(rateLimiter);
 
@@ -26,6 +31,14 @@ app.use(rateLimiter);
 // })
 
 app.use("/api/notes", notesRoutes)
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("*", (req, res) =>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+    })
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
